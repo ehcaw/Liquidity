b5306249e366835ab16d7f9db67f560929fac988
ManasC478 <manas.chougule@gmail.com>
2025-03-03 11:45:37 -0800

    fix: lint check

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -9,33 +9,34 @@ export const updateSession = async (request: NextRequest) => {
       },
     });
 
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          getAll() {
-            return request.cookies.getAll();
-          },
-          setAll(cookiesToSet) {
-            cookiesToSet.forEach(({ name, value }) =>
-              request.cookies.set(name, value),
-            );
-            response = NextResponse.next({
-              request,
-            });
-            cookiesToSet.forEach(({ name, value, options }) =>
-              response.cookies.set(name, value, options),
-            );
-          },
-        },
-      },
-    );
+    // const supabase = createServerClient(
+    //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    //   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    //   {
+    //     cookies: {
+    //       getAll() {
+    //         return request.cookies.getAll();
+    //       },
+    //       setAll(cookiesToSet) {
+    //         cookiesToSet.forEach(({ name, value }) =>
+    //           request.cookies.set(name, value),
+    //         );
+    //         response = NextResponse.next({
+    //           request,
+    //         });
+    //         cookiesToSet.forEach(({ name, value, options }) =>
+    //           response.cookies.set(name, value, options),
+    //         );
+    //       },
+    //     },
+    //   },
+    // );
 
     // do user authorization
 
     return response;
   } catch (e) {
+    console.log(e);
     // supabase client not working
     return NextResponse.next({
       request: {
