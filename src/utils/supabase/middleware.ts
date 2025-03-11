5a46162524b62f9bf87a176b3715e7e40fcd0faf
ManasC478 <manas.chougule@gmail.com>
2025-03-11 15:02:41 -0700

    feat: user auth middleware

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,38 +1,43 @@
+import { createServerClient } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
 export const updateSession = async (request: NextRequest) => {
   try {
-    const response = NextResponse.next({
+    let response = NextResponse.next({
       request: {
         headers: request.headers,
       },
     });
 
-    // const supabase = createServerClient(
-    //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    //   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    //   {
-    //     cookies: {
-    //       getAll() {
-    //         return request.cookies.getAll();
-    //       },
-    //       setAll(cookiesToSet) {
-    //         cookiesToSet.forEach(({ name, value }) =>
-    //           request.cookies.set(name, value),
-    //         );
-    //         response = NextResponse.next({
-    //           request,
-    //         });
-    //         cookiesToSet.forEach(({ name, value, options }) =>
-    //           response.cookies.set(name, value, options),
-    //         );
-    //       },
-    //     },
-    //   },
-    // );
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return request.cookies.getAll();
+          },
+          setAll(cookiesToSet) {
+            cookiesToSet.forEach(({ name, value }) =>
+              request.cookies.set(name, value),
+            );
+            response = NextResponse.next({
+              request,
+            });
+            cookiesToSet.forEach(({ name, value, options }) =>
+              response.cookies.set(name, value, options),
+            );
+          },
+        },
+      },
+    );
 
     // do user authorization
-
+    const { data: { session } } = await supabase.auth.getSession();
+    if (!session) {
+      response = NextResponse.redirect(new URL("/signin", request.url));
+    }
+    
     return response;
   } catch (e) {
     console.log(e);
