e4e512c4e39b03eabb08a385812bfdb49df09169
ManasC478 <manas.chougule@gmail.com>
2025-03-30 11:23:05 -0700

    feat: transaction api

---
+++ src/app/api/transactions/route.ts
@@ -0,0 +1,73 @@
+import { deposit, transfer, withdraw } from "@/services/banking/account";
+import { createTransaction } from "@/services/banking/transaction";
+import { ClientError, ServerError } from "@/utils/exceptions";
+import { PostTransactionRequestBody } from "@/utils/zod/transaction";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  let body;
+  try {
+    body = PostTransactionRequestBody.parse(await req.json());
+  } catch (error) {
+    return Response.json(
+      { error },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  try {
+    let transaction = {};
+    switch (body.transaction_type) {
+      case "Deposit":
+        transaction = await deposit(body.src_account_number, body.amount);
+        break;
+      case "Withdrawal":
+        transaction = await withdraw(body.src_account_number, body.amount);
+        break;
+      case "Transfer":
+        if (!body.dest_account_number) {
+          throw new ClientError("No destination account specified", 400);
+        }
+        transaction = await transfer(
+          body.src_account_number,
+          body.dest_account_number,
+          body.amount,
+        );
+        break;
+      case "Payment":
+        break;
+    }
+
+    return Response.json(
+      { data: transaction },
+      {
+        status: 201,
+      },
+    );
+  } catch (error) {
+    console.log(error);
+    if (error instanceof ClientError) {
+      return Response.json(
+        { error: error.message },
+        {
+          status: error.status,
+        },
+      );
+    } else if (error instanceof ServerError) {
+      return Response.json(
+        { error: error.message },
+        {
+          status: error.status,
+        },
+      );
+    }
+    return Response.json(
+      { error: error },
+      {
+        status: 500,
+      },
+    );
+  }
+}
