71e72fbe3bcea5cb2b52427c103d41b6a9e76592
ManasC478 <manas.chougule@gmail.com>
2025-03-30 11:24:46 -0700

    feat: transactions page

--- src/app/transfers/page.tsx
+++ src/app/transfers/page.tsx
@@ -1,4 +1,7 @@
-import TransferForm from "@/components/transfer-form"
+import DepositForm from "@/components/deposit-form";
+import TransferForm from "@/components/transfer-form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import WithdrawForm from "@/components/withdraw-form";
 import { Database } from "@/types/db";
 import { fetchData } from "@/utils/fetch";
 import { isAuthenticated } from "@/utils/isAuthenticated";
@@ -7,18 +10,33 @@ type Account = Database["public"]["Tables"]["accounts"]["Row"];
 
 export default async function TransfersPage() {
   await isAuthenticated();
-  const accounts = await fetchData<Account[]>('/api/account')
+  const accounts = await fetchData<Account[]>("/api/account");
   return (
     <div className="container mx-auto max-w-3xl py-6">
       <div className="flex flex-col gap-6">
         <div className="flex flex-col gap-2">
-          <h1 className="text-3xl font-bold tracking-tight">Transfers</h1>
-          <p className="text-muted-foreground">Schedule payments or create transfers between accounts</p>
+          <h1 className="text-3xl font-bold tracking-tight">Transactions</h1>
+          <p className="text-muted-foreground">
+            Transfer, deposit, and withdraw money between accounts
+          </p>
         </div>
-
-        <TransferForm accounts={accounts} />
+        <Tabs defaultValue="transfer">
+          <TabsList>
+            <TabsTrigger value="transfer">Transfer</TabsTrigger>
+            <TabsTrigger value="deposit">Deposit</TabsTrigger>
+            <TabsTrigger value="withdraw">Withdraw</TabsTrigger>
+          </TabsList>
+          <TabsContent value="transfer">
+            <TransferForm accounts={accounts} />
+          </TabsContent>
+          <TabsContent value="deposit">
+            <DepositForm accounts={accounts} />
+          </TabsContent>
+          <TabsContent value="withdraw">
+            <WithdrawForm accounts={accounts} />
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
-  )
+  );
 }
-
