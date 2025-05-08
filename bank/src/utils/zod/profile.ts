191df66ab9048557f52fa5495d2c2f52e106e6b2
ehcaw <ryannguyenc@gmail.com>
2025-05-07 18:33:37 -0700

    removed email field from profile request body to prevent backdoor email changing attacks

--- bank/src/utils/zod/profile.ts
+++ bank/src/utils/zod/profile.ts
@@ -3,10 +3,9 @@ import { z } from "zod";
 export const ProfileRequestBody = z.object({
   first_name: z.string(),
   last_name: z.string(),
-  email: z.string(),
   phone: z.string(),
   street: z.string(),
   city: z.string(),
   state: z.string(),
-  zipcode: z.string()
+  zipcode: z.string(),
 });
